7ef00677beb11594e6303e577e3da94e9c56180c
Grojman <gomisromanjorge@gmail.com>
2025-11-12 18:26:21 +0100

    Cambios en la barra lateral izquierda

--- js/sharedcontent/left-menu-column.js
+++ js/sharedcontent/left-menu-column.js
@@ -1,5 +1,21 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () =>{
-    
+    document.querySelector('#go-repair-catalog').addEventListener('click', () => {
+        //CARGAR CONTENIDO DE LA ZONA CLICKADA
+    })
+
+    document.querySelector('#go-my-products').addEventListener('click', () => {
+        //CARGAR CONTENIDO DE LA ZONA CLICKADA
+
+    })
+
+    document.querySelector('#go-my-repairs').addEventListener('click', () => {
+        //CARGAR CONTENIDO DE LA ZONA CLICKADA
+
+    })
+
+    document.querySelector('#go-my-tutorials').addEventListener('click', () => {
+        //CARGAR CONTENIDO DE LA ZONA CLICKADA
+    })
 })
